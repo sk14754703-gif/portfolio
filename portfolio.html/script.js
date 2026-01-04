9f02b45e82cb012d3c426f7982c892bfacda4b04
sk14754703-gif <sk14754703@gmail.com>
2026-01-04 23:52:06 +0530

    Add files via upload

---
+++ portfolio.html/script.js
@@ -0,0 +1,15 @@
+// Scroll Animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".hidden").forEach(el => observer.observe(el));
+
+// Contact Form
+function submitForm() {
+    alert("Thank you! Your message has been sent.");
+}
